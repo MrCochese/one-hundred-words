3fb7e1457f2a6394d002b1d25e5c752bab5b20e0
MrCochese <rick.bolton@gmail.com>
2015-06-10 23:09:17 +0100

    Added validation on entry post

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,35 @@ var router = express.Router();
 var entries = require('../data/entries');
 
 function render(req, res, next) {
-  var model = { words: '', entries: entries.list() };
+  var words = req.body.words || '';
+  var count = (words.match(/\S+/g) || []).length;
+
+  var model = { words: words, count: count, entries: entries.list() };
   res.render('index', model);
 };
 
 function insert(req, res, next) {
-  entries.add({ words: req.body.words, date: new Date(), count: req.body.words.match(/\S+/g).length });
+  var words = req.body.words || '';
+  var count = (words.match(/\S+/g) || []).length;
+
+  var model = { words: words, count: count, entries: entries.list() };
+
+  if (count < 100) {
+  	model.error = 'Needs more words'
+    return res.render('index', model);
+  }
+
+  entries.add({ words: req.body.words, date: new Date(), count: count });
   next();
+}
+
+function redirect(req, res, next) {
+  return res.redirect('/');
 }
 
 /* GET home page. */
 router.get('/', render);
 
-router.post('/', insert, render);
+router.post('/', insert, redirect);
 
 module.exports = router;
